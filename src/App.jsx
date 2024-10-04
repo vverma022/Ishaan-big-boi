b01fab456c45d9b3445bfc32d809cb0d9d01015a
Vasu Verma <vermavasu069@gmail.com>
2024-10-05 01:35:42 +0530

    Eshaaan Big Boi

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route>
+    </Routes>
+    </BrowserRouter>
+   
+  )
+}
+
+export default App
